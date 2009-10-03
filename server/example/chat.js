e8d44be7cee4b17b71f3319ed33cb5f573d7e1f3
Felix Geisendörfer <felix@debuggable.com>
2009-10-03 15:14:11 +0200

    More cleanup

--- server/example/chat.js
+++ server/example/chat.js
@@ -1,10 +1,11 @@
-var utils = require('/utils.js');
 var nodeChannel = require('../lib/node-channel.js');
 
 var server = nodeChannel.createServer();
 server.listen(8000);
 
 var channel = server.createChannel('foobar');
+
+// Delete messages from the chat history after 10 seconds
 channel.monitor.addListener('emit', function() {
 	setTimeout(function() {
 		channel.history.shift();
