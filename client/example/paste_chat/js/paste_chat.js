11381d57eb08a223ba395ae3c992e6f95b501ee6
Felix Geisendörfer <felix@debuggable.com>
2009-10-29 20:47:38 +0100

    Tabs to 2-space indention

--- client/example/paste_chat/js/paste_chat.js
+++ client/example/paste_chat/js/paste_chat.js
@@ -1,130 +1,130 @@
 $(function() {
-	var here = parseUri(window.location.href);
-	var server = $.nodeChannel.server('http://'+here.host+':8001/');
+  var here = parseUri(window.location.href);
+  var server = $.nodeChannel.server('http://'+here.host+':8001/');
 
-	server.request('get', '/')
-		.addCallback(function() {
-			$('.start button').fadeIn();
-		})
-		.addErrback(function() {
-			var failDogs = [
-				'http://lh3.ggpht.com/SergioAlex76/SO-B3KhnCyI/AAAAAAAAAKs/Kqb-ZxjiD80/fail-dog-ball%5B2%5D.jpg',
-				'http://images.triplem.com.au/2009/05/28/195337/fail-dog-24-600x400.jpg',
-				'http://data.tumblr.com/zcTqHiK8c5mqoszxmemvnwtF_400.jpg',
-				'http://farm3.static.flickr.com/2014/2263874070_55958e6727.jpg',
-				'http://untitled00.files.wordpress.com/2009/03/fail-dog.jpg',
-				'http://farm4.static.flickr.com/3271/2298205761_299e5e7706.jpg',
-				'http://aggregatemadbox.com/bloggregate/wp-content/uploads/2008/03/fail_dogs.jpg'
-			];
+  server.request('get', '/')
+    .addCallback(function() {
+      $('.start button').fadeIn();
+    })
+    .addErrback(function() {
+      var failDogs = [
+        'http://lh3.ggpht.com/SergioAlex76/SO-B3KhnCyI/AAAAAAAAAKs/Kqb-ZxjiD80/fail-dog-ball%5B2%5D.jpg',
+        'http://images.triplem.com.au/2009/05/28/195337/fail-dog-24-600x400.jpg',
+        'http://data.tumblr.com/zcTqHiK8c5mqoszxmemvnwtF_400.jpg',
+        'http://farm3.static.flickr.com/2014/2263874070_55958e6727.jpg',
+        'http://untitled00.files.wordpress.com/2009/03/fail-dog.jpg',
+        'http://farm4.static.flickr.com/3271/2298205761_299e5e7706.jpg',
+        'http://aggregatemadbox.com/bloggregate/wp-content/uploads/2008/03/fail_dogs.jpg'
+      ];
 
-			var key = Math.floor(Math.random() * failDogs.length);
-			var $failDog = $('<img />')
-				.attr('src', failDogs[key]);
-			var $error = $('<p class="error"/>')
-				.text('Sorry, but our systems are currently down : (')
-				.add($failDog)
-				.insertAfter('.start button');
-		});
+      var key = Math.floor(Math.random() * failDogs.length);
+      var $failDog = $('<img />')
+        .attr('src', failDogs[key]);
+      var $error = $('<p class="error"/>')
+        .text('Sorry, but our systems are currently down : (')
+        .add($failDog)
+        .insertAfter('.start button');
+    });
 
-	var channelId = window.location.hash.replace(/\?.*$/, '').substr(1);
-	if (channelId) {
-		var c = server.connectChannel(channelId)
-		startChat(c);
-	}
+  var channelId = window.location.hash.replace(/\?.*$/, '').substr(1);
+  if (channelId) {
+    var c = server.connectChannel(channelId)
+    startChat(c);
+  }
 
-	var channel, buffer = [], user;
-	function startChat(c) {
-		channel = c;
+  var channel, buffer = [], user;
+  function startChat(c) {
+    channel = c;
 
-		window.location.hash = '#'+c.id;
+    window.location.hash = '#'+c.id;
 
-		$('.start').remove();
-		$('.chat').fadeIn();
-		$('.chat input').val(here.source+'#'+channel.id);
+    $('.start').remove();
+    $('.chat').fadeIn();
+    $('.chat input').val(here.source+'#'+channel.id);
 
-		channel.addListener('message', function(message) {
-			for (i = 0; i < buffer.length; i++) {
-				if (buffer[i] === message.text) {
-					buffer.splice(i, 1);
-					return;
-				}
-			}
-			addMessage(message);
-		});
+    channel.addListener('message', function(message) {
+      for (i = 0; i < buffer.length; i++) {
+        if (buffer[i] === message.text) {
+          buffer.splice(i, 1);
+          return;
+        }
+      }
+      addMessage(message);
+    });
 
-		channel.addListener('join', function(message) {
-			var $li = $('<li/>').text(message.user);
-			console.log($('.chat .right ul'), $li, 'fun', message);
-			$('.chat .right ul').append($li);
-			addNote(message);
-		});
+    channel.addListener('join', function(message) {
+      var $li = $('<li/>').text(message.user);
+      console.log($('.chat .right ul'), $li, 'fun', message);
+      $('.chat .right ul').append($li);
+      addNote(message);
+    });
 
-		channel.since = 0;
-		channel.listen();
+    channel.since = 0;
+    channel.listen();
 
-		changeUser();
-	}
+    changeUser();
+  }
 
-	function changeUser() {
-		setTimeout(function() {
-			user = prompt('Please enter your name to participate in this chat:');
-			if (!user) {
-				return changeUser();
-			}
+  function changeUser() {
+    setTimeout(function() {
+      user = prompt('Please enter your name to participate in this chat:');
+      if (!user) {
+        return changeUser();
+      }
 
-			channel.emit('join', {user: user, text: user+' has entered the room'});
-		}, 200);
-	}
+      channel.emit('join', {user: user, text: user+' has entered the room'});
+    }, 200);
+  }
 
-	function submitMessage() {
-		var $message = $('.chat .message textarea');
-		var message = $message.val();
-		$message.val('');
+  function submitMessage() {
+    var $message = $('.chat .message textarea');
+    var message = $message.val();
+    $message.val('');
 
-		addMessage({user: user, text: message});
-		buffer.push(message);
+    addMessage({user: user, text: message});
+    buffer.push(message);
 
-		channel.emit('message', {user: user, text: message});
-	}
+    channel.emit('message', {user: user, text: message});
+  }
 
-	function addMessage (message) {
-		var $li = $('<li/>');
-		$li.text(message.text);
-		$li.prepend($('<strong/>').text(message.user+': '));
-		$('.chat .log').append($li);
-	};
+  function addMessage (message) {
+    var $li = $('<li/>');
+    $li.text(message.text);
+    $li.prepend($('<strong/>').text(message.user+': '));
+    $('.chat .log').append($li);
+  };
 
-	function addNote(note) {
-		var $li = $('<li/>');
-		$li.append($('<em/>').text(note.text));
-		$('.chat .log').append($li);
-	};
+  function addNote(note) {
+    var $li = $('<li/>');
+    $li.append($('<em/>').text(note.text));
+    $('.chat .log').append($li);
+  };
 
-	$('.start button').click(function() {
-		server
-			.createChannel()
-			.addErrback(function(e) {
-				alert('Could not create channel, reason: '+"\n\n"+JSON.stringify(e));
-			})
-			.addCallback(function(channel) {
-				startChat(channel);
-			});
-		return false;
-	});
+  $('.start button').click(function() {
+    server
+      .createChannel()
+      .addErrback(function(e) {
+        alert('Could not create channel, reason: '+"\n\n"+JSON.stringify(e));
+      })
+      .addCallback(function(channel) {
+        startChat(channel);
+      });
+    return false;
+  });
 
-	$('.header a').click(function() {
-		window.location.href = window.location.href.replace(/#.*$/, '');
-		return false;
-	});
+  $('.header a').click(function() {
+    window.location.href = window.location.href.replace(/#.*$/, '');
+    return false;
+  });
 
-	$('.chat .message textarea').keypress(function(e) {
-		if (e.keyCode == 13) {
-			submitMessage();
-			return false;
-		}
-	});
+  $('.chat .message textarea').keypress(function(e) {
+    if (e.keyCode == 13) {
+      submitMessage();
+      return false;
+    }
+  });
 
-	$('.chat .message button').click(function(e) {
-		submitMessage();
-	});
+  $('.chat .message button').click(function(e) {
+    submitMessage();
+  });
 });
--- client/lib/jquery.node-channel.js
+++ client/lib/jquery.node-channel.js
@@ -1,300 +1,300 @@
 // Some node.js idioms for the browser
 try {
-	window.node = {};
+  window.node = {};
 } catch (e) {
-	alert('Error: Could not export window.node namespace!');
+  alert('Error: Could not export window.node namespace!');
 }
 
 // A few functions I ported from node.js to be used in a browser environment
 (function(node) {
-	node.inherits = function(ctor, superCtor) {
-		var tempCtor = function(){};
-		tempCtor.prototype = superCtor.prototype;
-		ctor.super_ = superCtor.prototype;
-		ctor.prototype = new tempCtor();
-		ctor.prototype.constructor = ctor;
-	};
+  node.inherits = function(ctor, superCtor) {
+    var tempCtor = function(){};
+    tempCtor.prototype = superCtor.prototype;
+    ctor.super_ = superCtor.prototype;
+    ctor.prototype = new tempCtor();
+    ctor.prototype.constructor = ctor;
+  };
 
-	node.EventEmitter = function() {
-		this._listeners = {};
-	}
+  node.EventEmitter = function() {
+    this._listeners = {};
+  }
 
-	node.EventEmitter.prototype.addListener = function(event, listener) {
-		this.listeners(event).push(listener);
-		return this;
-	};
+  node.EventEmitter.prototype.addListener = function(event, listener) {
+    this.listeners(event).push(listener);
+    return this;
+  };
 
-	node.EventEmitter.prototype.listeners = function(event) {
-		return this._listeners[event] = this._listeners[event] || [];
-	};
+  node.EventEmitter.prototype.listeners = function(event) {
+    return this._listeners[event] = this._listeners[event] || [];
+  };
 
-	node.EventEmitter.prototype.emit = function(event) {
-		var listeners = this.listeners(event);
-		var args = Array.prototype.slice.call(arguments);
+  node.EventEmitter.prototype.emit = function(event) {
+    var listeners = this.listeners(event);
+    var args = Array.prototype.slice.call(arguments);
 
-		args.shift();
-		for (var i = 0; i < listeners.length; i++) {
-			listeners[i].apply(this, args);
-		}
-	};
+    args.shift();
+    for (var i = 0; i < listeners.length; i++) {
+      listeners[i].apply(this, args);
+    }
+  };
 
-	node.Promise = function() {
-		node.EventEmitter.call(this);
-	};
-	node.inherits(node.Promise, node.EventEmitter);
+  node.Promise = function() {
+    node.EventEmitter.call(this);
+  };
+  node.inherits(node.Promise, node.EventEmitter);
 
-	node.Promise.prototype.addCallback = function(fn) {
-		return this.addListener('success', fn);
-	}
+  node.Promise.prototype.addCallback = function(fn) {
+    return this.addListener('success', fn);
+  }
 
-	node.Promise.prototype.addErrback = function(fn) {
-		return this.addListener('error', fn);
-	}
+  node.Promise.prototype.addErrback = function(fn) {
+    return this.addListener('error', fn);
+  }
 
-	node.Promise.prototype.emitSuccess = function() {
-		var args = Array.prototype.slice.call(arguments);
-		args.unshift('success');
-		this.emit.apply(this, args);
-	};
+  node.Promise.prototype.emitSuccess = function() {
+    var args = Array.prototype.slice.call(arguments);
+    args.unshift('success');
+    this.emit.apply(this, args);
+  };
 
-	node.Promise.prototype.emitError = function() {
-		var args = Array.prototype.slice.call(arguments);
-		args.unshift('error');
-		this.emit.apply(this, args);
-	};
+  node.Promise.prototype.emitError = function() {
+    var args = Array.prototype.slice.call(arguments);
+    args.unshift('error');
+    this.emit.apply(this, args);
+  };
 })(window.node);
 
 (function($) {
-	var nodeChannel = $.nodeChannel = {};
+  var nodeChannel = $.nodeChannel = {};
 
-	nodeChannel.uuid = function() {
-		var uuid = '';
-		for (i = 0; i < 32; i++) {
-			uuid += Math.floor(Math.random() * 16).toString(16);
-		}
-		return uuid;
-	};
+  nodeChannel.uuid = function() {
+    var uuid = '';
+    for (i = 0; i < 32; i++) {
+      uuid += Math.floor(Math.random() * 16).toString(16);
+    }
+    return uuid;
+  };
 
-	nodeChannel.request = function(method, options) {
-		if (method == 'get') {
-			return nodeChannel._jsonp(options);
-		} else {
-			return nodeChannel._iframe(method, options);
-		}
-	};
+  nodeChannel.request = function(method, options) {
+    if (method == 'get') {
+      return nodeChannel._jsonp(options);
+    } else {
+      return nodeChannel._iframe(method, options);
+    }
+  };
 
-	nodeChannel._jsonp = function(options) {
-		var request = new node.Promise();
+  nodeChannel._jsonp = function(options) {
+    var request = new node.Promise();
 
-		var self = this;
-		$.jsonp({
-			url: options.uri,
-			data: options.data,
-			callbackParameter: 'callback',
-			timeout: options.timeout || 5000,
-			success: function(r){
-				request.emitSuccess(r);
-			},
-			error: function(xOptions, status) {
-				request.emitError(xOptions, status)
-			}
-		});
-		return request;
-	};
+    var self = this;
+    $.jsonp({
+      url: options.uri,
+      data: options.data,
+      callbackParameter: 'callback',
+      timeout: options.timeout || 5000,
+      success: function(r){
+        request.emitSuccess(r);
+      },
+      error: function(xOptions, status) {
+        request.emitError(xOptions, status)
+      }
+    });
+    return request;
+  };
 
-	var _counter = 0;
-	nodeChannel._iframe = function(method, options) {
-		var request = new node.Promise();
+  var _counter = 0;
+  nodeChannel._iframe = function(method, options) {
+    var request = new node.Promise();
 
-		var requestDone = false;
-		setTimeout(function() {
-			if (requestDone == false) {
-				request.emitError('timeout');
-			}
-		}, options.timeout || 5000);
+    var requestDone = false;
+    setTimeout(function() {
+      if (requestDone == false) {
+        request.emitError('timeout');
+      }
+    }, options.timeout || 5000);
 
-		var $form = $('<form enctype="multipart/form-data" />')
-			.attr('action', options.uri)
-			.attr('target', 'node-channel-iframe-'+_counter)
-			.attr('method', method.toUpperCase())
-			.appendTo('body')
-			.hide();
+    var $form = $('<form enctype="multipart/form-data" />')
+      .attr('action', options.uri)
+      .attr('target', 'node-channel-iframe-'+_counter)
+      .attr('method', method.toUpperCase())
+      .appendTo('body')
+      .hide();
 
-		if (options.fetch) {
-			options.data = $.extend({}, options.data, {'_request_id': nodeChannel.uuid()})
-		}
+    if (options.fetch) {
+      options.data = $.extend({}, options.data, {'_request_id': nodeChannel.uuid()})
+    }
 
-		$('<textarea name="data" />')
-			.text(JSON.stringify(options.data))
-			.prependTo($form);
+    $('<textarea name="data" />')
+      .text(JSON.stringify(options.data))
+      .prependTo($form);
 
-		var $iframe = $('<iframe id="node-channel-iframe-'+_counter+'" name="node-channel-iframe-'+_counter+'"/>')
-			.appendTo('body')
-			.hide();
+    var $iframe = $('<iframe id="node-channel-iframe-'+_counter+'" name="node-channel-iframe-'+_counter+'"/>')
+      .appendTo('body')
+      .hide();
 
-		$iframe.bind('load', function() {
-			$iframe.remove();
-			$form.remove();
+    $iframe.bind('load', function() {
+      $iframe.remove();
+      $form.remove();
 
-			if (!options.fetch) {
-				requestDone = true;
-				return request.emitSuccess();
-			}
+      if (!options.fetch) {
+        requestDone = true;
+        return request.emitSuccess();
+      }
 
-			var response = nodeChannel.request('get', {
-				uri: options.fetch+'/_response',
-				data: {'_request_id': options.data._request_id}
-			});
-			response.addErrback(function(r) {
-				requestDone = true;
-				request.emitError('response-error');
-			});
+      var response = nodeChannel.request('get', {
+        uri: options.fetch+'/_response',
+        data: {'_request_id': options.data._request_id}
+      });
+      response.addErrback(function(r) {
+        requestDone = true;
+        request.emitError('response-error');
+      });
 
-			response.addCallback(function(r) {
-				requestDone = true;
+      response.addCallback(function(r) {
+        requestDone = true;
 
-				if (r.error) {
-					return request.emitError(r.error);
-				}
+        if (r.error) {
+          return request.emitError(r.error);
+        }
 
-				request.emitSuccess(r);
-			});
-		});
-		$form.submit();
+        request.emitSuccess(r);
+      });
+    });
+    $form.submit();
 
-		_counter++;
-		return request;
-	};
+    _counter++;
+    return request;
+  };
 
-	nodeChannel.server = function(uri) {
-		var channel = new nodeChannel.Server(uri);
-		return channel;
-	};
+  nodeChannel.server = function(uri) {
+    var channel = new nodeChannel.Server(uri);
+    return channel;
+  };
 
 
-	nodeChannel.Server = function(uri) {
-		node.EventEmitter.call(this);
+  nodeChannel.Server = function(uri) {
+    node.EventEmitter.call(this);
 
-		this.uri = uri.replace(/\/+$/, '');
-	};
-	node.inherits(nodeChannel.Server, node.EventEmitter);
+    this.uri = uri.replace(/\/+$/, '');
+  };
+  node.inherits(nodeChannel.Server, node.EventEmitter);
 
-	nodeChannel.Server.prototype.createChannel = function() {
-		var self = this;
+  nodeChannel.Server.prototype.createChannel = function() {
+    var self = this;
 
-		var request = nodeChannel.request('post', {
-			uri: this.uri+'/_create_channel',
-			data: {},
-			fetch: this.uri
-		});
+    var request = nodeChannel.request('post', {
+      uri: this.uri+'/_create_channel',
+      data: {},
+      fetch: this.uri
+    });
 
-		var promise = new node.Promise();
-		request.addErrback(function(e) {
-			promise.emitError(e);
-		});
+    var promise = new node.Promise();
+    request.addErrback(function(e) {
+      promise.emitError(e);
+    });
 
-		request.addCallback(function(r) {
-			var channel = new nodeChannel.Channel(self, r.id);
-			promise.emitSuccess(channel);
-		});
-		return promise;
-	};
+    request.addCallback(function(r) {
+      var channel = new nodeChannel.Channel(self, r.id);
+      promise.emitSuccess(channel);
+    });
+    return promise;
+  };
 
-	nodeChannel.Server.prototype.connectChannel = function(id) {
-		var channel = new nodeChannel.Channel(this, id);
-		return channel;
-	};
+  nodeChannel.Server.prototype.connectChannel = function(id) {
+    var channel = new nodeChannel.Channel(this, id);
+    return channel;
+  };
 
-	nodeChannel.Server.prototype.request = function(method, uri, options) {
-		options = $.extend({uri: this.uri+uri}, options || {});
-		return nodeChannel.request(method, options);
-	};
+  nodeChannel.Server.prototype.request = function(method, uri, options) {
+    options = $.extend({uri: this.uri+uri}, options || {});
+    return nodeChannel.request(method, options);
+  };
 
-	nodeChannel.Channel = function(server, id) {
-		node.EventEmitter.call(this);
+  nodeChannel.Channel = function(server, id) {
+    node.EventEmitter.call(this);
 
-		this.monitor = new node.EventEmitter();
+    this.monitor = new node.EventEmitter();
 
-		this.server = server;
-		this.id = id;
-		this.since = (+new Date());
+    this.server = server;
+    this.id = id;
+    this.since = (+new Date());
 
-		this.timeout = 45 * 1000;
-		this.pause =  1 * 1000;
-	};
-	node.inherits(nodeChannel.Channel, node.EventEmitter);
+    this.timeout = 45 * 1000;
+    this.pause =  1 * 1000;
+  };
+  node.inherits(nodeChannel.Channel, node.EventEmitter);
 
-	nodeChannel.Channel.prototype.emit = function(name) {
-		var args = Array.prototype.slice.call(arguments);
-		args.shift();
+  nodeChannel.Channel.prototype.emit = function(name) {
+    var args = Array.prototype.slice.call(arguments);
+    args.shift();
 
-		// Causes the event to be emitted locally, but not send to the remote channel
-		if (name === false) {
-			node.EventEmitter.prototype.emit.apply(this, args);
-			return;
-		}
+    // Causes the event to be emitted locally, but not send to the remote channel
+    if (name === false) {
+      node.EventEmitter.prototype.emit.apply(this, args);
+      return;
+    }
 
-		var event = {
-			name: name,
-			args: args
-		};
-		var options = {
-			uri: this.server.uri+'/'+this.id,
-			data: [event]
-		};
-		var request = nodeChannel.request('post', options);
+    var event = {
+      name: name,
+      args: args
+    };
+    var options = {
+      uri: this.server.uri+'/'+this.id,
+      data: [event]
+    };
+    var request = nodeChannel.request('post', options);
 
-		var self = this;
-		request.addListener('success', function(r) {
+    var self = this;
+    request.addListener('success', function(r) {
 
-		});
+    });
 
-		request.addListener('error', function(r) {
-			// Handle
-		});
+    request.addListener('error', function(r) {
+      // Handle
+    });
 
-		node.EventEmitter.prototype.emit.apply(this, args);
-	};
+    node.EventEmitter.prototype.emit.apply(this, args);
+  };
 
-	nodeChannel.Channel.prototype.listen = function(pause, timeout) {
-		this.timeout = (timeout === undefined) ? this.timeout : timeout;
-		this.pause = (pause === undefined) ? this.pause : pause;
+  nodeChannel.Channel.prototype.listen = function(pause, timeout) {
+    this.timeout = (timeout === undefined) ? this.timeout : timeout;
+    this.pause = (pause === undefined) ? this.pause : pause;
 
-		var options = {
-			uri: this.server.uri+'/'+this.id,
-			data: {since: this.since}
-		};
-		var request = nodeChannel.request('get', options);
+    var options = {
+      uri: this.server.uri+'/'+this.id,
+      data: {since: this.since}
+    };
+    var request = nodeChannel.request('get', options);
 
-		var self = this;
-		request.addListener('success', function(r) {
-			self._emitHistory(r.history);
+    var self = this;
+    request.addListener('success', function(r) {
+      self._emitHistory(r.history);
 
-			setTimeout(function() {
-				self.listen();
-			}, self.pause);
-		});
+      setTimeout(function() {
+        self.listen();
+      }, self.pause);
+    });
 
-		request.addListener('error', function(xOptions, status) {
-			if (status != 'timeout') {
-				return;
-			}
+    request.addListener('error', function(xOptions, status) {
+      if (status != 'timeout') {
+        return;
+      }
 
-			setTimeout(function() {
-				self.listen();
-			}, self.pause);
-		});
-	};
+      setTimeout(function() {
+        self.listen();
+      }, self.pause);
+    });
+  };
 
-	nodeChannel.Channel.prototype._emitHistory = function(history) {
-		this.since = history[history.length - 1].time;
+  nodeChannel.Channel.prototype._emitHistory = function(history) {
+    this.since = history[history.length - 1].time;
 
-		var self = this;
-		$.each(history, function(i, event) {
-			var args = event.args;
-			args.unshift(event.name);
-			args.unshift(false);
-			self.emit.apply(self, args);
-		});
-	};
+    var self = this;
+    $.each(history, function(i, event) {
+      var args = event.args;
+      args.unshift(event.name);
+      args.unshift(false);
+      self.emit.apply(self, args);
+    });
+  };
 })(jQuery);
